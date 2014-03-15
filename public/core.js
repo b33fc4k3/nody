6cafa8def673c6530a61ea0e76f1d7df46635e6d
Marten <marten.p.rieger@googlemail.com>
2014-03-15 16:25:01 +0100

    working https redirect, bowers angular + tutorial, ng-animate, traditional routes

--- public/core.js
+++ public/core.js
@@ -39,4 +39,49 @@ function mainController($scope, $http) {
 			});
 	};
 
+	// testing angular: http://www.ng-newsletter.com/posts/beginner2expert-data-binding.html
+	var updateClock = function() {
+    		$scope.clock = new Date();
+  	};
+  	var timer = setInterval(function() {
+    		$scope.$apply(updateClock);
+  	}, 1000);
+  	updateClock();
 }
+
+// // ng-animate and ng-route -ing here ...
+// // define our application and pull in ngRoute and ngAnimate
+// var animateApp = angular.module('animateApp', ['ngRoute', 'ngAnimate']);
+// 
+// // ROUTING ===============================================
+// // set our routing for this application
+// // each route will pull in a different controller
+// animateApp.config(function($routeProvider) {
+//     $routeProvider
+//     	.when('/', {
+//     		templateUrl: 'page-home.html',
+//             controller: 'mainController'
+//     	})
+// 
+//     	.when('/about', {
+//     		templateUrl: 'page-about.html',
+//             controller: 'aboutController'
+//     	})
+// 
+//     	.when('/contact', {
+//     		templateUrl: 'page-contact.html',
+//             controller: 'contactController'
+//     	});
+// });
+// 
+// animateApp.controller('mainController', function($scope) {
+//     $scope.pageClass = 'page-home';
+// });
+// 
+// animateApp.controller('aboutController', function($scope) {
+//     $scope.pageClass = 'page-about';
+// });
+// 
+// animateApp.controller('contactController', function($scope) {
+//     $scope.pageClass = 'page-contact';
+// });
--- server.js
+++ server.js
@@ -2,18 +2,13 @@
 var express  = require('express');
 var app      = express(); 								// create our app w/ express
 var mongoose = require('mongoose'); 					// mongoose for mongodb
-// https node-fs
+// https node-fs ... all part of node, no need to npm install but fs aka node-fs is to be downloaded
 var https    = require('https');
-//var http     = require('http');
+var http     = require('http');
 var fs       = require('fs');
-// failed to load c++ bson extension ...
-// https://stackoverflow.com/questions/21656420/failed-to-load-c-bson-extension
-// When you installed the mongoose module by npm, it hasn't build bson module within it's forlder.  see the file 
-// 'node_modules/mongoose/node_modules/mongodb/node_modules/bson/ext/index.js 
-// bson = require('../build/Release/bson');
-// So just change it to
-// bson = require('bson');
-// and npm install bson
+
+var httpPort  = 8080;
+var httpsPort = 8081;
 
 // configuration =================
 // TODO
@@ -21,11 +16,25 @@ var fs       = require('fs');
 // coffeescript
 
 // https node-fs
+//http://www.gettingcirrius.com/2012/06/securing-nodejs-and-express-with-ssl.html !!!!
+//http://greengeckodesign.com/blog/2013/06/15/creating-an-ssl-certificate-for-node-dot-js/
+//http://www.benjiegillam.com/2012/06/node-dot-js-ssl-certificate-chain/
 var options = {
-	key:  fs.readFileSync('key.pem'),
-	cert: fs.readFileSync('cert.pem')
+	//key:  fs.readFileSync('key.pem'),
+	//cert: fs.readFileSync('cert.pem')
+	key:  fs.readFileSync('./privatekey.pem'),
+	cert: fs.readFileSync('./certificate.pem')
+	//ca:
+	//requestCert: true,
+	//rejectUnauthorized: false,
+	//passphrase: "mrieger"
 };
 
+//var app = module.exports = express.createServer(options); // ???
+//var app = require('express').createServer(options); // ???
+
+//var httpsServer      = express.createServer(options);								// create our app w/ express
+//var app      = express.createServer(options); 								// create our app w/ express
 mongoose.connect('mongodb://localhost:27017/myNode'); 	// connect to mongoDB database on modulus.io
 
 app.configure(function() {
@@ -33,7 +42,19 @@ app.configure(function() {
 	app.use(express.logger('dev')); 						// log every request to the console
 	app.use(express.bodyParser()); 							// pull information from html in POST
 	app.use(express.methodOverride()); 						// simulate DELETE and PUT
+	//testweise
+	//app.use(express.session( { secret: 'mrieger' } ));
+	//app.use(express.cookieParser());
+	//app.use(app.router);
 });
+
+//httpsServer.configure(function() {
+//	app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+//	app.use(express.logger('dev')); 						// log every request to the console
+//	app.use(express.bodyParser()); 							// pull information from html in POST
+//	app.use(express.methodOverride()); 						// simulate DELETE and PUT
+//});
+
 
 // define model =================
 var Todo = mongoose.model('Todo', {
@@ -104,19 +125,104 @@ app.delete('/api/todos/:todo_id', function(req, res) {
 // ANGULAR =====================================================================
 // frontend
 // application -------------------------------------------------------------
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
 	res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
+//app.get('/', function(req, res) {
+//	res,redirect('https://localhost:8081/public/index.html');
+//	//res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+//});
+
+
+app.get('/animate', function(req, res) {
+	res.sendfile('./public/index_animate.html');
+});
+
+app.get('/info', function(req, res) {
+	console.log(req);
 });
 
 // listen (start app with node server.js) ======================================
 //var app = module.exports = express.createServer();
-https.createServer(options, app).listen(8081);
+//https.createServer(options, app).listen(8081);
+//https.createServer(options, app).listen(80);
+
 //app.listen(8080);
-console.log("App listening on port 8080");
 
 // https node-fs
 //var a = https.createServer(options, function (req, res) {
 //  res.writeHead(200);
 //  res.end("hello world\n");
 //}).listen(8000);
-//https.createServer(options, app).listen(8081);
+
+////=======================================================================================
+//// set up server to listen on httpPort and redirect anyone to https-express-app
+var httpProxy = express();
+httpProxy.get('*', function(req, res) {
+	res.redirect('https://localhost:8081'+req.url);
+})
+httpProxy.listen(httpPort);
+
+// // TODO
+// // those two together serve node and mongo
+// // why does https not serve!??
+// var httpProxy = http.createServer(app);
+// httpProxy.listen(httpPort);
+
+
+////=======================================================================================
+//// start up app server
+var httpsServer = https.createServer(options,app);
+//https.createServer(options, function(req, res) {
+//	app.handle(req, res);
+//}).listen(httpsPort);
+httpsServer.listen(httpsPort);
+////var httpsServer = express();
+
+//=======================================================================================
+// shout out to user
+console.log("App listening on port: " + httpPort + "\nHTTPS on: " + httpsPort);
+
+app.listen(8070);
+
+exports = module.exports = app; 						// expose app
+
+//http.createServer(app).listen(8070);
+
+//http.get('*',function(req,res){  
+//    //res.redirect('https://marten.uk.to'+req.url)
+//    res.redirect('https://localhost:8081'+req.url)
+//})
+//http.listen(8080);
+
+// https redirect as seen here:
+// http://stackoverflow.com/questions/15813677/https-redirection-for-all-routes-node-js-express-security-concerns
+// http://stackoverflow.com/questions/7450940/automatic-https-connection-redirect-with-node-js-express
+// http://stackoverflow.com/questions/10697660/force-ssl-with-expressjs-3
+// 01. does work ... but https doesnt serve mongo and animate, only static
+//var http = express.createServer();
+//http.get('*',function(req,res){  
+//    //res.redirect('https://marten.uk.to'+req.url)
+//    res.redirect('https://localhost:8081'+req.url)
+//})
+//http.listen(8080);
+
+// 02.
+//function requireHTTPS(req, res, next) {
+//    if (!req.secure) {
+//        //FYI this should work for local development as well
+//        return res.redirect('https://' + req.get('host') + req.url);
+//    }
+//    next();
+//}
+
+//app.use(requireHTTPS);
+//
+//// oder
+//app.use(function(req, res, next) {
+//  if(!req.secure) {
+//    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+//  }
+//  next();
+//});
